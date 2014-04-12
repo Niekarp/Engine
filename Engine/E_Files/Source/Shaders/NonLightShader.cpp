680d08e373dee990ffc38a5cc80b1c9d10778ba0
Niekarp <skrzynka255@gmail.com>
2014-04-13 00:26:57 +0200

    xx

--- Engine/E_Files/Source/Shaders/NonLightShader.cpp
+++ Engine/E_Files/Source/Shaders/NonLightShader.cpp
@@ -5,8 +5,8 @@ void NonLightShader::Load(){
 	Data fragment_shader = RcLoad(RE_NON_LIGHT_FRAGMENT_SHADER);
 	GLuint ivs = glCreateShader(GL_VERTEX_SHADER);
 	GLuint ifs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(ivs, 1, (const char**)vertex_shader.res, (GLint*)&vertex_shader.res_size);
-	glShaderSource(ifs, 1, (const char**)fragment_shader.res, (GLint*)&fragment_shader);
+	glShaderSource(ivs, 1, (const GLchar**)vertex_shader.res, (GLint*)&vertex_shader.res_size);
+	glShaderSource(ifs, 1, (const GLchar**)fragment_shader.res, (GLint*)&fragment_shader.res_size);
 	
 	glAttachShader(program, ivs);
 	glAttachShader(program, ifs);
