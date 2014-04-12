aded9fa82a1469f348d98c8322baded97443bfb9
Niekarp <skrzynka255@gmail.com>
2014-04-12 16:13:02 +0200

    CzarnyPoniedzialek

--- Engine/E_Files/Headers/ResourcesLoader/RcLoader.h
+++ Engine/E_Files/Headers/ResourcesLoader/RcLoader.h
@@ -4,6 +4,6 @@
 
 struct Data{
 	LPVOID res;
-	HRSRC res_size;
+	DWORD res_size;
 };
 Data RcLoad(int RESOURCE_ID);
--- Engine/E_Files/Source/ResourcesLodaer/RcLoader.cpp
+++ Engine/E_Files/Source/ResourcesLodaer/RcLoader.cpp
@@ -3,7 +3,7 @@
 Data RcLoad(int RESOURCE_ID){
 	Data data;
 	data.res = LockResource(LoadResource(0, FindResource(0, MAKEINTRESOURCE(RESOURCE_ID), RT_RCDATA)));
-	data.res_size = FindResource(0, MAKEINTRESOURCE(RESOURCE_ID), RT_RCDATA);
+	data.res_size = SizeofResource(0, FindResource(0, MAKEINTRESOURCE(RESOURCE_ID), RT_RCDATA));
 	if (!data.res || !data.res_size)throw "Wystapil problem przy uzyskiwaniu dostepu do zasobow NonLightShader'a";
 	return data;
 }
